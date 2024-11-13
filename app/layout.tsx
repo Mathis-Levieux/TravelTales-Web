34af3bd842033a755ade3e5a95e81b036cbde6eb
Mathis Levieux <mathis.levieux1@gmail.com>
2024-11-13 10:17:36 +0100

    added logo, re added force dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import type { Metadata } from 'next';
 import { Plus_Jakarta_Sans, Inter } from 'next/font/google';
 import './globals.css';
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
 import Header from './ui/header';
+import Image from 'next/image';
+import logoSvg from "@/public/logo.svg";
 
 export default function Home() {
-
   return (
     <>
       <Header isLandingPage={true} />
-      <main className="">
-        <div className="flex justify-center">
-          <h1 className="text-4xl">Welcome to TravelTales</h1>
+      <main className="w-full flex justify-center mt-16 px-4 sm:mt-16">
+        <div className="flex flex-col items-center p-6 bg-white/50 shadow-lg rounded-lg max-w-2xl m-auto">
+          {/* Logo */}
+          <div className="flex-shrink-0 mb-4 sm:mb-0">
+            <Image
+              src={logoSvg}
+              alt="Logo de l'application"
+              width={128}
+              height={128}
+              className="w-24 h-24 sm:w-48 sm:h-48"
+            />
+          </div>
+
+          {/* Texte de bienvenue */}
+          <div className="text-center sm:text-left sm:ml-6">
+            <h1 className="text-xl sm:text-2xl font-bold text-gray-800 text-center mt-10">
+              Bienvenue sur TravelTales !
+            </h1>
+            <p className="mt-2 text-gray-600">
+              Simplifiez vos voyages avec notre plateforme moderne et intuitive. Planifiez vos itinéraires, gérez votre budget et préparez votre valise pour partir l’esprit tranquille.
+            </p>
+          </div>
         </div>
       </main>
     </>
